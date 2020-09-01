4eaf6ed15c81bbaa553770d68ea85e0e40f8c77b
semio <semio@posteo.net>
2020-09-01 21:30:13 +0800

    remove daff settings, use new functions from ddf_utils

--- etl/scripts/update_source.py
+++ etl/scripts/update_source.py
@@ -1,22 +1,12 @@
 # -*- coding: utf-8 -*-
 
-import requests as r
+from ddf_utils.factory.common import download
 
 
 url = 'http://fenixservices.fao.org/faostat/static/bulkdownloads/FAOSTAT.zip'
 out_file = '../source/FAOSTAT.zip'
 
-def download():
-    res = r.get(url, stream=True)
-    with open(out_file, 'wb') as f:
-        for chunk in res.iter_content(chunk_size=10240):
-            if chunk:
-                f.write(chunk)
-        f.close()
-    return
-
-
 if __name__ == '__main__':
     print('downloading file to ' + out_file)
-    download()
+    download(url, out_file)
     print('Done.')
